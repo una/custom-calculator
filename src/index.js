b72fbe020b5a9816dad89ba9792865b70e7f8328
Una Kravets <unakravets@google.com>
2025-06-28 23:04:08 -0400

    adds updated icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorkerRegistration'; // create-react-app's new name for this file
+import * as serviceWorker from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,5 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// Change to .register() to enable PWA features
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.unregister();
